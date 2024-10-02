ee274903026896d1121a7311d02103ce29f03cbb
claudiamendez7 <claudiamendezt19@gmail.com>
2024-10-02 18:58:07 -0500

    creating const

--- alura_midi/js/app.js
+++ alura_midi/js/app.js
@@ -1,1 +1,10 @@
-document.querySelector(".tecla_pom");
+const teclaPom = document.querySelector(".tecla_pom");
+const teclaClap = document.querySelector(".tecla_clap");
+const teclaTim = document.querySelector(".tecla_tim");
+const teclaPuff = document.querySelector(".tecla_puff");
+const teclaSplash = document.querySelector(".tecla_splash");
+const teclaToque = document.querySelector(".tecla_toque");
+const teclaPling = document.querySelector(".tecla_pling");
+const teclaOom = document.querySelector(".tecla_oom");
+
+const listaDeTeclas = document.querySelectorAll(".tecla");
