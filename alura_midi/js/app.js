745ac693ae48a6a3fcb20adc693f999a72d31e96
claudiamendez7 <claudiamendezt19@gmail.com>
2024-10-07 15:54:24 -0500

    applying audios

--- alura_midi/js/app.js
+++ alura_midi/js/app.js
@@ -1,10 +1,18 @@
-const teclaPom = document.querySelector(".tecla_pom");
-const teclaClap = document.querySelector(".tecla_clap");
-const teclaTim = document.querySelector(".tecla_tim");
-const teclaPuff = document.querySelector(".tecla_puff");
-const teclaSplash = document.querySelector(".tecla_splash");
-const teclaToque = document.querySelector(".tecla_toque");
-const teclaPling = document.querySelector(".tecla_pling");
-const teclaOom = document.querySelector(".tecla_oom");
+function playSound(idAudioElement) {
+  document.querySelector(idAudioElement).play();
+}
+const mediaList = document.querySelectorAll(".tecla");
 
-const listaDeTeclas = document.querySelectorAll(".tecla");
+let contador = 0;
+
+while (contador < 9) {
+  const media = mediaList[contador];
+  const instrument = media.classList[1];
+
+  const idAudio = `#sonido_${instrument}`;
+  console.log(idAudio);
+  media.onclick = function () {
+    playSound(idAudio);
+  };
+  contador = contador + 1;
+}
