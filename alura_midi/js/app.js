9dfc2f7cb2306cf6a6f8f8197b9102f276bc4c54
claudiamendez7 <claudiamendezt19@gmail.com>
2024-10-07 18:24:39 -0500

    final commit

--- alura_midi/js/app.js
+++ alura_midi/js/app.js
@@ -6,10 +6,10 @@ const mediaList = document.querySelectorAll(".tecla");
 for (let contador = 0; contador < mediaList.length; contador++) {
   const media = mediaList[contador];
   const instrument = media.classList[1];
-  console.log(contador);
+  //   console.log(contador);
 
   const idAudio = `#sonido_${instrument}`;
-  console.log(idAudio);
+  //   console.log(idAudio);
   media.onclick = function () {
     playSound(idAudio);
   };
@@ -17,7 +17,7 @@ for (let contador = 0; contador < mediaList.length; contador++) {
     if (event.code === "Space" || event.code === "Enter") {
       media.classList.add("activa");
     }
-    console.log(event.code === "Space" || event.code === "Enter");
+    // console.log(event.code === "Space" || event.code === "Enter");
   };
   media.onkeyup = function () {
     media.classList.remove("activa");
